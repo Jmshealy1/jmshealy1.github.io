c82a6f2d9c9d2c683e795ad095eeef5f2fde93da
Jmshealy1 <jmshealy@email.sc.edu>
2025-04-01 08:14:10 -0400

    home page

--- csce242/exercises/04_js/js/json-breweries.js
+++ csce242/exercises/04_js/js/json-breweries.js
@@ -12,7 +12,44 @@ const getMap = async(latitude, longitude) => {
     }
 };
 
-const showBreweries = async9() => {
-    const breweriesSection = documents.getElementById("breweries");
-    const breweries = await getBrew
-}
+const getBreweries = async() => {
+    const url = "https://api.openbrewerydb.org/breweries";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showBreweries = async() => {
+    const breweriesSection = document.getElementById("breweries");
+    const breweries = await getBreweries();
+    
+    breweries.forEach(async(brewery)=> {
+        const section = document.createElement("section");
+        section.classList.add("brewery");
+        breweriesSection.append(section);
+
+        const a = document.createElement("a");
+        a.href = brewery.website_url;
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = brewery.name;
+        a.append(h3);
+        
+        const p = document.createElement("p");
+        p.innerHTML = brewery.brewery_type + " brewery";
+        section.append(p);
+
+        const iframe = document.createElement("iframe");
+        iframe.src= await getMap(brewery.latitude, brewery.longitude);
+        section.append(iframe);
+
+    });
+    
+};
+
+showBreweries();
